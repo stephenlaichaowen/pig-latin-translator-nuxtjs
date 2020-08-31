d14306c5b13518309e97bbb17b06aef0b81f492a
stephenlai <stephenlai2015@gmail.com>
2020-08-31 13:33:12 +0800

    commit

---
+++ layouts/default.vue
@@ -0,0 +1,82 @@
+<template>
+  <div>
+    <PageLoader />
+    <nuxt />
+  </div>
+</template>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: sans-serif;
+}
+
+body {
+  max-width: 1920px;
+  margin: auto;
+  padding: 2rem;
+  min-height: 100vh;
+  background: #aaa;
+  color: rgba(255, 255, 255, 0.7);
+  background-color: #6760aa;
+}
+
+.container {
+  max-width: 650px;
+  margin: 0 auto;
+  /* padding: 0 25px; */
+}
+
+.header {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 50px;
+  margin-bottom: 25px;
+}
+
+.header__title {
+  font-size: 3rem;
+}
+
+.header__icon {
+  max-height: 100px;
+  margin-right: 20px;
+}
+
+.text-input__input {
+  width: 100%;
+  resize: none;
+  padding: 15px;
+  border-radius: 2px;
+  border: none;
+  box-shadow: 1px 1px 2px #7f9fd0;
+  font-size: 14px;
+  color: #6760aa;
+}
+
+.text-input__input:focus {
+  outline: #7f9fd0 auto 2px;
+}
+
+.text-output {
+  background-color: rgba(255, 255, 255, 0.7);
+  color: #6760aa;
+  padding: 15px;
+}
+
+@media (max-width: 470px) {
+  .header {
+    width: 100%;
+  }
+  .header__icon {
+    width: 80px;
+    height: 80px;
+  }
+  .header__title {
+    font-size: 2rem;
+  }
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="container">
+    <header class="header">
+      <img
+        src="https://www.goodfreephotos.com/albums/vector-images/pig-vector-art.png"
+        class="header__icon"
+        alt="Pig Icon"
+      />
+      <h1 class="header__title">Pig Latin Translator</h1>
+    </header>
+    <div class="text-input">
+      <textarea v-model="inputText" class="text-input__input" id="textInput" cols="30" rows="10"></textarea>
+    </div>
+    <div class="text-output" id="textOutput">{{ outputLatin }}</div>
+  </div>
+</template>
+
+<script>
+const vowels = ['e', 'i', 'a', 'o', 'u']
+const ending = 'ay'
+export default {
+  data() {
+    return {
+      inputText: ''
+    }
+  },
+  computed: {
+    outputLatin() {
+      const output = this.inputText
+        .split(' ')
+        .map(word => {
+          if (vowels.includes(word[0])) {
+            return word + ending
+          } else {
+            return word
+          }
+          let consonantsPrefix = ''
+          for (let i = 0; i < word.length; i++) {
+            const char = word[i]
+            if (vowels.includes(char)) {
+              break
+            }
+            consonantsPrefix += char
+          }
+          return (
+            word.substring(consonantsPrefix.length) + consonantsPrefix + ending
+          )
+        }).join(' ')
+      return output
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
